2a71ba1e8b33b68cdee12e6dcdaefcd113532dfb
Sumy Ganas <aksmy.997@gmail.com>
2025-07-21 11:27:26 -0700

    update config object

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,14 @@ import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  apiKey: "AIzaSyAyVHWeBUKUD7LV6MsVwCnyVR4lzFFvNHk",
+  authDomain: "alpine-figure-414421.firebaseapp.com",
+  databaseURL: "https://alpine-figure-414421-default-rtdb.firebaseio.com",
+  projectId: "alpine-figure-414421",
+  storageBucket: "alpine-figure-414421.firebasestorage.app",
+  messagingSenderId: "65938638422",
+  appId: "1:65938638422:web:59cfdc6ea5ecba80a75df7",
+  measurementId: "G-GZVSWFBF1G"
 };
 
 
