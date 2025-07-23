f7d5cf5ac060a25bae540af1520410f9efc807c3
Sumy Ganas <aksmy.997@gmail.com>
2025-07-23 10:37:03 -0700

    create TOS link in footer

---
+++ src/footer/footer.jsx
@@ -0,0 +1,37 @@
+import { React, useState } from 'react';
+import '../styles/App.css'
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import TermsOfUse from './Termsofuse.jsx';
+import Nav from 'react-bootstrap/Nav';
+
+const FooterBar = () => {
+    const [activeComponent, setActiveComponent] = useState(null);
+    const componentMap = {
+        tos: <TermsOfUse />
+      };
+    
+     return (<footer className="blog-footer">
+    <Navbar className="bg-body-tertiary" sticky="bottom">
+      <Container>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-center">
+          <Navbar.Text>
+          <TermsOfUse/>
+          <br/> 
+            <Nav.Item>
+              Copyright © 2024 Sumy Ganas   
+            </Nav.Item>
+          <br/>
+          <Nav.Item>
+          <div style={{textAlign:"center", fontSize:"12px"}}><a href="https://www.flaticon.com/">Stickers created by Stickers - Flaticon</a></div>
+          </Nav.Item>
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  </footer>)
+}
+
+
+export default FooterBar;
