c7941b08e5aa0d3dea684f6502218d266a54d2af
Sumy Ganas <aksmy.997@gmail.com>
2025-12-19 20:06:56 +0000

    remove unused imports

--- src/ai-app/FormComponent.jsx
+++ src/ai-app/FormComponent.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import '../styles/App.css';
 import Form from 'react-bootstrap/Form';
 
--- src/ai-app/ResponseComponent.jsx
+++ src/ai-app/ResponseComponent.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import '../styles/App.css';
 import TopDeals  from './topDeals';
 import PrefDeals from './prefDeals';
