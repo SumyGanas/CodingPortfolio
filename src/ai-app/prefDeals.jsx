f7584b2b8de3ff1ddcf0923931724a2ca55ee404
Sumy Ganas <aksmy.997@gmail.com>
2026-01-02 06:26:37 +0000

    fix syntax for parsing incomplete deals

--- src/ai-app/prefDeals.jsx
+++ src/ai-app/prefDeals.jsx
@@ -1,12 +1,13 @@
 import '../styles/App.css';
 
 const PrefDeals = ({response}) => {
+
     return ( <>
     <div className='ai-title-header'> Based on your preferences, here are the top deals at Ulta</div>
     <div className='ai-subtitle-header'>PS: Click on the product name to navigate to the product website</div>
     <div className='aiListDiv'>
-    {Object.keys(response || {}).map((categoryName) => {
-    const products = response?.[categoryName] || [];
+    {Object.entries(response || {}).map(([categoryName, products]) => {
+    // const products = response?.[categoryName] || [];
         return(
             <>
                 <div className='category-header'>
@@ -14,23 +15,40 @@ const PrefDeals = ({response}) => {
                   </div>
                   <div className='aiListDiv'>
                     <ol>
-                        {products.map((p) => (
-                          <li className='aiListNumber' key={p.url || p.name || i}>
+                        {products.map((p) => {
+                          const hasAllFields = [p.url, p.name, p.list_price, p.sale_price, p.product_relevance_for_customer].every((val) => val != null);
+
+                          if (!hasAllFields) return null;
+                          return (
+                          <li className='aiListNumber' key={p.url || p.name}>
                             <div className='aiListItem'>
                           <div className='aiListProdName'>
                             <a href={p.url} className='prodLink' target="_blank" rel="noopener noreferrer">
                             <p>{p.name}</p>
                             </a></div>
                             <div className='prices'>
-                            <span><s>{"$"+p.list_price}</s> </span>  
-                            <span> {"$"+p.sale_price}</span>
+                             {p.sale_price != p.list_price ? <span><s>{
+                            new Intl.NumberFormat('en-US', {
+                                style: 'currency',
+                                currency: 'USD',
+                                }).format(p.list_price)
+                            }
+                            </s> </span> : null} 
+                            <span> {
+                            new Intl.NumberFormat('en-US', {
+                                style: 'currency',
+                                currency: 'USD',
+                                }).format(p.sale_price)
+                            }
+                            </span>
                             </div>
                           </div>
                                 <div className='aiListContent'>
                                 <p>{p.product_relevance_for_customer}</p>  
                                 </div>
-                            </li>
-                        ))}
+                            </li> 
+                            );
+                          })}
                     </ol>
                     </div>
                     </> 
